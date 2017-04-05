145892925b008f2cb8f2f8adfc9f414861af7705
Ketav Bhatt <ketavbhatt@gmail.com>
2017-04-05 16:47:46 +0530

    Update views.py

--- solution/views.py
+++ solution/views.py
@@ -45,7 +45,7 @@ def hello():
 	AUTH_TOKEN = 'YOUR TWILIO AUTH TOKEN'
 	client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
 
-	client.messages.create(to = 'NUMBER YOU WANT TO SEND MESSAGE',from_ = 'YOUR TWILIO NUMBER', body = 'Your BODY OF MESSAGE',)
+	client.messages.create(to = 'NUMBER YOU WANT TO SEND MESSAGE',from_ = 'YOUR TWILIO NUMBER', body = 'BODY OF MESSAGE',)
 	return 0
 
 
